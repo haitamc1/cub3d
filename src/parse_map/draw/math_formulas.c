6475640ba213f6ed5ac442eb56a684ad1b7059bd
Haitam Chahid <hchahid@e1r6p5.1337.ma>
2023-01-10 12:22:34 +0100

    added function to calculate ray lenghts

--- src/parse_map/draw/math_formulas.c
+++ src/parse_map/draw/math_formulas.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   math_formulas.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arouzen <arouzen@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: hchahid <hchahid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 13:33:06 by hchahid           #+#    #+#             */
-/*   Updated: 2023/01/06 17:13:07 by arouzen          ###   ########.fr       */
+/*   Updated: 2023/01/10 12:21:32 by hchahid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 double	pythagore(double adjacent, double opposite)
 {
 	return (sqrt(pow(opposite, 2) + pow(adjacent, 2)));
+}
+
+double	ray_lenght(double ray_x, double ray_y, double player_x, double player_y)
+{
+	return (sqrt(pow(ray_x - player_x, 2) + pow(ray_y - player_y, 2)));
 }
